f9966d624e7082ebdfbf057c5c1a8d97381714a7
Mifa43 <mifa43kotez@gmail.com>
2021-08-10 16:03:09 +0200

    return the readable value

--- contact/models.py
+++ contact/models.py
@@ -20,11 +20,26 @@ class AdressEntery(models.Model):
     # def is_superclass(self):
     #     return self.CHOICES_GENDER in {self.MALE, self.FAMELE}
 
+    def __str__(self):
+        return self.name
+
 class Person(models.Model):
     person = models.ForeignKey(AdressEntery, on_delete=models.CASCADE)
     firstName = models.CharField(max_length = 120)
     lastName = models.CharField(max_length = 120)
 
+    def __str__(self):
+        return str(self.person)
+
 class Contact(models.Model):
     contact = models.ForeignKey(AdressEntery, on_delete=models.CASCADE)
     phoneNumber = models.IntegerField()
+
+    def __str__(self):
+        return str(self.contact)
+    #region fix
+    # This is the way we display our data from the table located in the admin panel,
+    #  and the shell in the docker container will not be returned to us anymore (table name objects (1))
+    #endregion
+
+   # name  gender  birthDate  active firstName lastName phoneNumber
