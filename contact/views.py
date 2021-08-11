803659441580c2853b7f51a1fd08d68c984b6237
Mifa43 <mifa43kotez@gmail.com>
2021-08-11 09:45:58 +0200

    definition for paths

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def add_contact(response):
+    pass
+
+def update_contact(response):
+    pass
+
+def list_of_contacts(response):
+    pass
+
+def delete(response):
+    pass
+
+def contact(response):
+    pass
+
+def person(response):
+    pass
+
+def addresses(response):
+    pass
