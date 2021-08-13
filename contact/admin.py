6c5fae247db4bc2cfe5b6ff1a2b328396ab69bf9
Mifa43 <mifa43kotez@gmail.com>
2021-08-13 21:31:32 +0200

    added comits for admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import AdressEntery, Person, Contact
 # Register your models here.
+#register your table on admin panel
 admin.site.register(AdressEntery) 
 admin.site.register(Person) 
 admin.site.register(Contact) 
