636c55d2ce57f2cda9eb36927107c09177a9452e
Mifa43 <mifa43kotez@gmail.com>
2021-08-13 20:31:03 +0200

    added commits for end point add-contact

--- contact/views.py
+++ contact/views.py
@@ -5,21 +5,23 @@ from .models import AdressEntery, Person, Contact
 # Create your views here.
 def add_contact(response):
     if response.method == 'POST':
-        form = CreateContact(response.POST)
-        if form.is_valid():
+        form = CreateContact(response.POST)     #capturing post requests from the form
+        if form.is_valid():     # validate form
             name, birth_date, first_name, last_name, phone_number = form.cleaned_data['name'], form.cleaned_data['birthDate'], form.cleaned_data['firstName'], form.cleaned_data['lastName'], form.cleaned_data['phoneNumber']
             gender = form.cleaned_data['gender']
             gender = dict(form.fields['gender'].choices)[gender]
+            #taking values from the form field
 
             add = AdressEntery(name=name, gender=gender.upper(), birthDate=birth_date)
             add.save()
             add.person_set.create(firstName=first_name, lastName=last_name)
             add.contact_set.create(phoneNumber=phone_number)
+            #adding a new contact to the table and relation and saving it
 
-        return HttpResponseRedirect("http://localhost:8001/api/add-contact/")
+        return HttpResponseRedirect("http://localhost:8001/api/add-contact/")   
     else:
-        form = CreateContact()
-    return render(response, 'main/add_contact.html', {"form": form})
+        form = CreateContact()  # if not post then just show form 
+    return render(response, 'main/add_contact.html', {"form": form})    # rendering template
 
 def list_of_contacts(response):
     lista = AdressEntery.objects
