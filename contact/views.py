4dd183c792dc7b54cffc09ef52347a01424d2681
Mifa43 <mifa43kotez@gmail.com>
2021-08-13 12:29:55 +0200

    a block has been added that captures changes and updates them

--- contact/views.py
+++ contact/views.py
@@ -36,10 +36,33 @@ def update_contact(response, id):
         form = OptionalForm(response.POST)
         if form.is_valid():
             name, birth_date, first_name, last_name, phone_number = form.cleaned_data['name'], form.cleaned_data['birthDate'], form.cleaned_data['firstName'], form.cleaned_data['lastName'], form.cleaned_data['phoneNumber']
-            get_contact = AdressEntery.objects.filter(id=37).update(name=name)
-            print(get_contact)
+            gender = form.cleaned_data['gender']
+            gender = dict(form.fields['gender'].choices)[gender]
+            if name and gender and birth_date and first_name and last_name and phone_number:
 
-        
+                update = AdressEntery.objects.filter(id=id).update(name=name, gender=gender.upper(), birthDate=birth_date)
+                update_rel = AdressEntery.objects.get(id=id)
+                update_rel.person_set.update(firstName=first_name, lastName=last_name)
+                update_rel.contact_set.update(phoneNumber=phone_number)
+            elif len(name) >= 1:
+                update = AdressEntery.objects.filter(id=id).update(name=name)
+            elif len(gender) >= 1:
+                update = AdressEntery.objects.filter(id=id).update(gender=gender.upper())
+            elif len(birth_date) >= 1:
+                update = AdressEntery.objects.filter(id=id).update(birthDate=birth_date)
+            elif len(first_name) >= 1:
+                update = AdressEntery.objects.get(id=id)
+                update.person_set.update(firstName=first_name)
+            elif len(last_name) >= 1:
+                update = AdressEntery.objects.get(id=id)
+                update.person_set.update(lastName=last_name)
+            elif len(phone_number) >= 1:
+                update = AdressEntery.objects.get(id=id)
+                update.contact_set.update(phoneNumber=phone_number)
+
+
+
+
         return HttpResponseRedirect("http://localhost:8001/api/list-of-contacts/")
     else:
         form = OptionalForm()
