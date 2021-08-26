7701b47cff57f20eee2db6a4214567cac60d4c13
Mifa43 <mifa43kotez@gmail.com>
2021-08-26 10:47:54 +0200

    added commits for registration

--- registration/views.py
+++ registration/views.py
@@ -3,9 +3,10 @@ from .forms import RegisterForm
 # Create your views here.
 def register(response):
     if response.method == 'POST':
-        form = RegisterForm(response.POST)
+        form = RegisterForm(response.POST)  #way of user registration
+        #we capture an entry from the form field
         if form.is_valid():
-            form.save()
+            form.save() #we add to the database
     else:
         form = RegisterForm()
 
