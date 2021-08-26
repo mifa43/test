467027fea7fdd1c3ac596230b83d23c8246e24a8
Mifa43 <mifa43kotez@gmail.com>
2021-08-26 10:51:14 +0200

    added commits for form

--- registration/forms.py
+++ registration/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import fields
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class RegisterForm(UserCreationForm):#the new class contains a reference to the jango register form
+    email = forms.EmailField()  #we add a new field
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = User 
+        fields = ['username', 'email', 'password1', 'password2']    #field display
