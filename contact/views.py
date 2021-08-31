7f7da5ee33886421f113ffecdebbb33ea7db6a9b
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 18:41:36 +0200

    removed spaces

--- contact/views.py
+++ contact/views.py
@@ -7,7 +7,7 @@ from .forms import CreateContact, OptionalForm
 from contact import models
 
 # <a href="{% url 'add-contacts' %}">New contact</a>
-#             <a href="{% url 'contact-list' %}">Contacts</a>       return to nav in base
+# <a href="{% url 'contact-list' %}">Contacts</a>       return to nav in base
 class Home(ListView):
     model = AdressEntery
     template_name = "main/home.html"
