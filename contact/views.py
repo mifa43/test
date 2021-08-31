d72087a80d7dfd8ad0a296caeaf2a2c23ad89b11
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 12:17:08 +0200

    added template name variable

--- contact/views.py
+++ contact/views.py
@@ -3,17 +3,23 @@ from .models import AdressEntery, Person, Contact
 from django.views import View
 
 class Home(View):
+    template_name ="main/home.html"
+
     def get(self, request):
-        return render(request, "main/home.html", {})
+        return render(request, self.template_name, {})
 
 class GetListOfContacts(View):
+    template_name = "main/contact_list.html"
+
     def get(self, request):
         contact_list = AdressEntery.objects.all()
         counter_for_active = AdressEntery.objects.filter(active=True).count()
-        return render(request, "main/contact_list.html", {"contact_list": contact_list, "counter": counter_for_active})
+        return render(request, self.template_name, {"contact_list": contact_list, "counter": counter_for_active})
 
 class FilterContacts(View):
+    template_name = "main/filter.html"
+
     def get(self, request):
         filter = AdressEntery.objects.filter()
         counter_for_active = AdressEntery.objects.filter(active=True).count()
-        return render(request, "main/filter.html", {"filter": filter.order_by("-birthDate"), "counter": counter_for_active})
+        return render(request, self.template_name, {"filter": filter.order_by("-birthDate"), "counter": counter_for_active})
