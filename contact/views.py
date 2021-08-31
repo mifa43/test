1b9a022df9874c9705ff53311d1d8a2e5f7c669d
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 10:53:56 +0200

    added class for displaying contacts

--- contact/views.py
+++ contact/views.py
@@ -4,5 +4,9 @@ from django.views import View
 
 class Home(View):
     def get(self, request):
-        
         return render(request, "main/home.html", {})
+
+class GetListOfContacts(View):
+    def get(self, request):
+        contact_list = AdressEntery.objects.all()
+        return render(request, "main/contact_list.html", {"contact_list": contact_list})
