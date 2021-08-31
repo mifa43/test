7d176db6f7798af51761afa3db5fac52e50ab3b0
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 19:06:11 +0200

    added commits

--- contact/views.py
+++ contact/views.py
@@ -7,7 +7,7 @@ from .forms import CreateContact, OptionalForm
 from contact import models
 
 # <a href="{% url 'add-contacts' %}">New contact</a>
-#      return to nav in base
+#      return to nav in base    <a href="{% url 'Filter' %}">
 class Home(ListView):   # this is class and it's return home page
     model = AdressEntery    # model 
     template_name = "main/home.html"        # template
@@ -15,4 +15,4 @@ class Home(ListView):   # this is class and it's return home page
 class GetListOfContacts(ListView):
     model = AdressEntery
     template_name = "main/contact_list.html"
-    context_object_name = "contact_list"
+    context_object_name = "contact_list" #  this will overwrite variabl in html. on first place is something_list
