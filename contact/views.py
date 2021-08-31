aabd13886f236fc97d67084bb48190306c06d24f
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 10:16:21 +0200

    deleted query because home is not needed

--- contact/views.py
+++ contact/views.py
@@ -4,5 +4,5 @@ from django.views import View
 
 class Home(View):
     def get(self, request):
-        contact = AdressEntery.objects.all()
-        return render(request, "main/home.html", {"contact": contact})
+        
+        return render(request, "main/home.html", {})
