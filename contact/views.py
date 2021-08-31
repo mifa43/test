2be849369b8c2ae544d06977c816839d6fc3a60a
Mifa43 <mifa43kotez@gmail.com>
2021-08-31 14:20:49 +0200

    Added helper class to type and save contact goal is to call views.py in class AddContacts

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import redirect, render
 from .models import AdressEntery, Person, Contact
 from django.views import View
 from .forms import CreateContact, OptionalForm
+#region block for helping classes
+class Add():
+    def __init__(self, name, gender, birth_date, first_name, last_name, phone_number):
+        self.name = name
+        self.gender = gender
+        self.birth_date = birth_date
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+
+    def contact(self):
+        add = AdressEntery(name=self.name, gender=self.gender.upper(), birthDate=self.birth_date)
+        add.save()
+        add.person_set.create(firstName=self.first_name, lastName=self.last_name)
+        add.contact_set.create(phoneNumber=self.phone_number)
+#endregion
+
 
 class Home(View):
     template_name ="main/home.html"
@@ -25,36 +42,20 @@ class FilterContacts(View):
         filter = AdressEntery.objects.filter()
         counter_for_active = AdressEntery.objects.filter(active=True).count()
         return render(request, self.template_name, {"filter": filter.order_by("-birthDate"), "counter": counter_for_active})
-        
-class Add():
-    def __init__(self, name, gender, birth_date, first_name, last_name, phone_number):
-        self.name = name
-        self.gender = gender
-        self.birth_date = gender
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-
-    def contact(self):
-        add = AdressEntery(name=self.name, gender=self.gender.upper(), birthDate=self.birth_date)
-        add.save()
-        add.person_set.create(firstName=self.first_name, lastName=self.last_name)
-        add.contact_set.create(phoneNumber=self.phone_number)
 
 class AddContacts(View):
     template_name = "main/add_contact.html"
     form = CreateContact
-
+    
     def get(self, request):
         return render(request, self.template_name, {"form": self.form})
 
     def post(self, request):
         form = self.form(request.POST)
         if form.is_valid():
-            all_fields = self.form.declared_fields.keys()
-            for i in all_fields:
-                s = form.cleaned_data[i]
-                print(s)
-            form.save()
-            return redirect("api/add-contacts/")
+            name, birth_date, first_name, last_name, phone_number = form.cleaned_data['name'], form.cleaned_data['birthDate'], form.cleaned_data['firstName'], form.cleaned_data['lastName'], form.cleaned_data['phoneNumber']
+            gender = form.cleaned_data['gender']
+            gender = dict(form.fields['gender'].choices)[gender]
+            Add(name, gender, birth_date, first_name, last_name, phone_number).contact()
+            return redirect("http://localhost:8001/api/add-contacts/")
         return render(request, self.template_name, {"form": self.form})
