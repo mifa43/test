83d37cdc0c21f126072c73ad68108e6195167b35
Mifa43 <mifa43kotez@gmail.com>
2021-09-03 12:59:53 +0200

    deleted commited old method for registration

--- registration/views.py
+++ registration/views.py
@@ -7,19 +7,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Register(CreateView):
     form_class = RegisterForm
-    #context_object_name = "form"
     template_name = "register/register.html"
     success_url = reverse_lazy("home")
-    # def form_valid(self, form):
-    #     form.instance.user = self.request.user
-    #     return super(Register, self).form_valid(form)
-# def register(response):
-#     if response.method == 'POST':
-#         form = RegisterForm(response.POST)  #way of user registration
-#         #we capture an entry from the form field
-#         if form.is_valid():
-#             form.save() #we add to the database
-#     else:
-#         form = RegisterForm()
-
-#     return render(response, "register/register.html", {"form": form})
