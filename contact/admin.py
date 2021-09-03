a49c921c22bb46401b19262f047cf323f02eafaa
Mifa43 <mifa43kotez@gmail.com>
2021-09-03 09:34:25 +0200

    removed old models

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import AdressEntery, Person, Contact
+from .models import AdressEntery
 # Register your models here.
 #register your table on admin panel
 admin.site.register(AdressEntery) 
-admin.site.register(Person) 
-admin.site.register(Contact) 
+# admin.site.register(Person) 
+# admin.site.register(Contact) 
