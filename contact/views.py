bcd04a2648c321cfd63549e631a0d3169904a978
Mifa43 <mifa43kotez@gmail.com>
2021-09-03 11:12:39 +0200

    explanatory comments for individual sections have been added

--- contact/views.py
+++ contact/views.py
@@ -14,7 +14,7 @@ class GetListOfContacts(ListView):  # return all contacts for current user
     model = AdressEntery
     context_object_name = "contact_list" #  this will overwrite variabl in html. on first place is something_list
     template_name = "main/contact_list.html"
-    def get_queryset(self):
+    def get_queryset(self): # This method queries the database and filters it
         queryset = super(GetListOfContacts, self).get_queryset()
         return queryset.filter(active=True, user=self.request.user)
     def get_context_data(self, *args, **kwargs):
@@ -29,27 +29,27 @@ class FilterContacts(ListView):
     ordering = "-birthDate"     #just like in old version of code ordering == order_by in methods
     def get_queryset(self): # query instance
         queryset = super(FilterContacts, self).get_queryset()   # super used to find the "parent class" and return its object
-        return queryset.filter(active=True, user=self.request.user) # return query set as filtrered data
-    def get_context_data(self, *args, **kwargs):
+        return queryset.filter(active=True, user=self.request.user) # return query set as filtrered data 
+    def get_context_data(self, *args, **kwargs):    # the way we use to fill in the values from get_queryset()
         context = super().get_context_data(*args, **kwargs) # when we said super().get_context_data(*args, **kwargs) inherits from get_context_data use ctrl + left click and see where it will take you
         context['counter'] = self.get_queryset().count()    # context['counter'] this is similar to what we used: return render(request, template, {"var": var})
         return context  # and pass to template
 
 class AddContacts(CreateView):
-    model = AdressEntery
-    fields = ("name", "gender", "birthDate", "firstName", "lastName", "phoneNumber")
+    model = AdressEntery    # db model
+    fields = ("name", "gender", "birthDate", "firstName", "lastName", "phoneNumber")    #these are the fields that will be displayed in our form
     template_name = "main/add_contact.html"
     context_object_name = "form" # html variabl form
     success_url = "/api/add-contacts/"  # redirect
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(AddContacts, self).form_valid(form)
+    def form_valid(self, form): # this is similar to what we used if form.is_valid() -> bool
+        form.instance.user = self.request.user # we assign a user for the field user in model
+        return super(AddContacts, self).form_valid(form)    # save form inputs
 
-class DeleteContact(DeleteView):
+class DeleteContact(DeleteView):    
     model = AdressEntery
     context_object_name = "obj"
     template_name = "main/adressentery_confirm_delete.html"
-    success_url = reverse_lazy("contact-list")
+    success_url = reverse_lazy("contact-list")  # this is redirected to a specific url for the parameter using the name expressed in urls .py
    
 class UpdateContact(UpdateView):
     model = AdressEntery
@@ -59,11 +59,7 @@ class UpdateContact(UpdateView):
     success_url = "http://localhost:8001/api/contact-list/"
 
 #region docs
-#1. allow_empty:
-#A boolean specifying whether to display the page if no objects are available. 
-#If this is False and no objects are available, the view will raise a 404 instead of displaying an empty page. By default, this is True.
-
-#2.read more about this dispatch:
+#1.read more about this dispatch:
 # https://docs.djangoproject.com/en/3.2/ref/class-based-views/base/#django.views.generic.base.View.dispatch
 # https://stackoverflow.com/questions/47808652/what-is-dispatch-used-for-in-django/47808940
 #endregion
