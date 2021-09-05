f1763e0caa7c7fb6e5903b53c6db008abf8bee03
Mifa43 <mifa43kotez@gmail.com>
2021-09-05 13:10:49 +0200

    added a way instead of deleting we put flag active false and do not show those people

--- contact/views.py
+++ contact/views.py
@@ -50,11 +50,14 @@ class DeleteContact(DeleteView):
     context_object_name = "obj"
     template_name = "main/adressentery_confirm_delete.html"
     success_url = reverse_lazy("contact-list")  # this is redirected to a specific url for the parameter using the name expressed in urls .py
-    def dispatch(self,*args,**kwargs):
-        return super(DeleteContact, self).dispatch(*args, **kwargs)
-    def get_queryset(self):
+    
+    def dispatch(self,request, *args, **kwargs):   #this method allows us to use methods such as (post, get, put ..) and allows us to return HTTP methods such as (response, redirect)
+        super(DeleteContact, self).dispatch(request)  #dispatch method will override the second method and allow us to do redirection
+        return redirect('/api/contact-list/')   #render(request, self.template_name, {})
+    def get_queryset(self, *args, **kwargs):
         queryset = super(DeleteContact, self).get_queryset()
-        return queryset.filter(id=self.kwargs['pk']).update(active=False)
+        queryset.filter(id=self.kwargs['pk']).update(active=False)
+        return queryset   # filter by clicked contact card get id and set active false
 
 class UpdateContact(UpdateView):
     model = AdressEntery
