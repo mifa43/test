3c813e0a6eb38a5a6c37965c37633876182b9b75
Mifa43 <mifa43kotez@gmail.com>
2021-09-21 13:43:05 +0200

    added class for geting phone number and message

--- socialMessage/views.py
+++ socialMessage/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView
-# Create your views here.
+from django.views.generic.base import TemplateView, View
+from contact.models import AdressEntery
+import webbrowser as web
+import time
 
+# Create your views here.
+# class Social():
+
+#     def __init__(self, phone, message):
+#         self.phone = phone
+#         self.message = message
+#     def whatsup(self):
+#         #num = "+381"+self.phone
+        
+#         web.open(f"https://web.whatsapp.com/send?phone=+381{self.phone}&text={self.message}")
+        
+#         time.sleep(5)
+       #"https://web.whatsapp.com/send?phone=+3810600977458&text=caooooo"
+class SendMessages(ListView):
+    model = AdressEntery
+    template_name = "main/sendMessage.html"
+    fields = ['phoneNumber']
+    
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(SendMessages, self).get_queryset()
+        phone = queryset.get(id=self.kwargs['pk'], active=True, user=self.request.user)
+        
+        if self.request.GET.get('send') == "Send":               
+            message = self.request.GET.get('text')
+           
+        return queryset
+    def render_to_response(self, context):
+        super().render_to_response(context)
+        return redirect("https://web.whatsapp.com/send?phone=+3810600977458&text=caooooo")
+         
+        # Cao ovo je djangfo contact book whatsup poruka!
