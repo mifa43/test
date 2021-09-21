df054cc3d3580b319510ac7f537e2cb9d6f5a05a
Mifa43 <mifa43kotez@gmail.com>
2021-09-21 13:44:02 +0200

    update new settings

--- Main/settings.py
+++ Main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'contact.apps.ContactConfig',
     'registration.apps.RegistrationConfig',
+    'socialMessage.apps.SocialmessageConfig'
 ]
 
 MIDDLEWARE = [
