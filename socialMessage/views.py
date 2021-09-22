73232477b608ab3abad1283b8e86012a85624349
Mifa43 <mifa43kotez@gmail.com>
2021-09-22 09:06:14 +0200

    deleted old clsss and imported send_mail

--- socialMessage/views.py
+++ socialMessage/views.py
@@ -1,39 +1,25 @@
+from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 from django.views.generic.base import TemplateView, View
 from contact.models import AdressEntery
-import webbrowser as web
-import time
+from django.core.mail import send_mail
 
-# Create your views here.
-# class Social():
-
-#     def __init__(self, phone, message):
-#         self.phone = phone
-#         self.message = message
-#     def whatsup(self):
-#         #num = "+381"+self.phone
-        
-#         web.open(f"https://web.whatsapp.com/send?phone=+381{self.phone}&text={self.message}")
-        
-#         time.sleep(5)
-       #"https://web.whatsapp.com/send?phone=+3810600977458&text=caooooo"
 class SendMessages(ListView):
     model = AdressEntery
     template_name = "main/sendMessage.html"
     fields = ['phoneNumber']
-    
+    context_object_name = "obj"
+
     def get_queryset(self, *args, **kwargs):
         queryset = super(SendMessages, self).get_queryset()
-        phone = queryset.get(id=self.kwargs['pk'], active=True, user=self.request.user)
+        self.usr = self.request.user
         
         if self.request.GET.get('send') == "Send":               
             message = self.request.GET.get('text')
-           
+            email = self.request.GET.get('email')
+            
         return queryset
-    def render_to_response(self, context):
-        super().render_to_response(context)
-        return redirect("https://web.whatsapp.com/send?phone=+3810600977458&text=caooooo")
-         
-        # Cao ovo je djangfo contact book whatsup poruka!
+    
