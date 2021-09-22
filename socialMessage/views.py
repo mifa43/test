d56ffdea50e61458def3fb91bae3b23ff087ea8d
Mifa43 <mifa43kotez@gmail.com>
2021-09-22 12:02:55 +0200

    addede commits

--- socialMessage/views.py
+++ socialMessage/views.py
@@ -14,21 +14,14 @@ class SendMessages(ListView):
     template_name = "main/sendMessage.html"
     fields = ['phoneNumber']
     context_object_name = "obj"
-
     def get_queryset(self, *args, **kwargs):
         queryset = super(SendMessages, self).get_queryset()
-        usr = self.request.user              
-        message = self.request.GET.get('text')
-        to_email = [self.request.GET.get('email'),]
-        subject = self.request.GET.get('subject')
-        user_password = os.getenv("DJANGO_EMAIL_PASSWORD")
-        connection = [usr.email, user_password, False,]
-        if self.request.GET.get('send') == "Send":
-            send_mail(subject, message, usr.email, to_email, connection)
+        usr = self.request.user  #get an email from the currently logged in user
+        message = self.request.GET.get('text')  #html input message
+        to_email = [self.request.GET.get('email'),] # input target email
+        subject = self.request.GET.get('subject')   #email subject/title
+        user_password = os.getenv("DJANGO_EMAIL_PASSWORD")#docker env get password
+        connection = [usr.email, user_password, False,] #connect to email, if it doesn't work, turn off the settings on the email (less security) and avast
+        if self.request.GET.get('send') == "Send":  #pressed button
+            send_mail(subject, message, usr.email, to_email, connection)    #send an emal
         return queryset
-     # self.message = self.request.GET.get('text')
-    #         self.to_email = [self.request.GET.get('email'),]
-    #         self.subject = self.request.GET.get('subject')
-    #         self.user_password = os.getenv("DJANGO_EMAIL_PASSWORD")
-    #         self.connection = [self.usr.email, self.user_password, False,]
-    # print(self.subject, self.message, self.usr.email, self.to_email, self.connection)
